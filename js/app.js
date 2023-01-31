558548dbf722cb8d727a40b2c739ad6c5cac3dee
lifeoncode <nduduzo@yellogarden.co.za>
2023-01-31 22:28:55 +0200

    image animations and hero section parallax

--- js/app.js
+++ js/app.js
@@ -12,5 +12,25 @@ const isVisible = (element) => {
 
 // attach scroll event on window
 window.addEventListener("scroll", (e) => {
-  console.log(isVisible(document.querySelector(".image")));
+  // hero section parallax effect
+  const heroSection = document.querySelector(".hero");
+  let scrollSpeed = heroSection.dataset.scrollspeed;
+  let scrollPosition = window.pageYOffset;
+  let scrollValue = scrollPosition * scrollSpeed;
+  heroSection.setAttribute("style", `transform: translateY(${scrollValue}px)`);
+
+  // every image reveals with animation when in full view
+  const images = document.querySelectorAll(".image");
+  images.forEach((img) => {
+    if (isVisible(img)) {
+      img.querySelector(".overlay").classList.add("open");
+    }
+  });
+  // when user scrolls all the way to the top
+  // re-apply image overlays
+  if (window.pageYOffset === 0) {
+    images.forEach((img) => {
+      img.querySelector(".overlay").classList.remove("open");
+    });
+  }
 });
