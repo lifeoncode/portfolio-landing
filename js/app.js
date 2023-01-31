905f8c227373369e68dfd8e4b14a56d958a92b55
lifeoncode <nduduzo@yellogarden.co.za>
2023-01-31 23:13:12 +0200

    text animations

--- js/app.js
+++ js/app.js
@@ -10,6 +10,38 @@ const isVisible = (element) => {
   );
 };
 
+// function for all element animations
+const elementAnimations = () => {
+  // every image reveals with animation when in full view
+  const images = document.querySelectorAll(".image");
+  images.forEach((img) => {
+    if (isVisible(img)) {
+      img.querySelector(".overlay").classList.add("open");
+    }
+  });
+
+  // heading text animation
+  const headings = document.querySelectorAll(".heading");
+  headings.forEach((heading) => {
+    if (isVisible(heading)) {
+      heading.querySelector("span").classList.add("show");
+    }
+  });
+
+  // paragraph text animaiton
+  const paragraphs = document.querySelectorAll("p");
+  paragraphs.forEach((paragraph) => {
+    if (isVisible(paragraph)) {
+      paragraph.querySelector("span").classList.add("show");
+    }
+  });
+};
+
+// run animations on content load
+window.addEventListener("load", (e) => {
+  elementAnimations();
+});
+
 // attach scroll event on window
 window.addEventListener("scroll", (e) => {
   // hero section parallax effect
@@ -19,13 +51,9 @@ window.addEventListener("scroll", (e) => {
   let scrollValue = scrollPosition * scrollSpeed;
   heroSection.setAttribute("style", `transform: translateY(${scrollValue}px)`);
 
-  // every image reveals with animation when in full view
-  const images = document.querySelectorAll(".image");
-  images.forEach((img) => {
-    if (isVisible(img)) {
-      img.querySelector(".overlay").classList.add("open");
-    }
-  });
+  // keep calling the element animations function on user scroll
+  elementAnimations();
+
   // when user scrolls all the way to the top
   // re-apply image overlays
   if (window.pageYOffset === 0) {
